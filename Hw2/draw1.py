4aa9935abca7479ba35a7c8e9d673e9081fd2325
vinhnq1011 <vinhnq.neu@gmail.com>
2020-05-19 14:02:45 +0700

    init git

--- Hw2/draw1.py
+++ Hw2/draw1.py
@@ -1,5 +1,7 @@
+
+# cách 1
 from turtle import *
-# speed(-1)
+speed(-1)
 color('red')
 forward(100)
 left(120)
@@ -8,7 +10,6 @@ for i in range(2):
     forward(100)
     left(120)
 
-#1
 for i in range(4):
     color('red')
     forward(100)
@@ -27,28 +28,32 @@ for i in range(6):
     forward(100)
     left(360/6)
 
-#2
-from turtle import *
-
-for edge in range(3, 6):
-  for i in range (edge):
-        forward(100)
-        left(360/edge)
-        if edge == 3 or edge == 5:
-            color('blue')
-        else:
-            color('red')
-
-color('red')
-forward(100)
-
-for j in range(5):
-    left(360/6)
-    forward(100)
-    color('red')
-
-left(60)    
 mainloop()
+
+
+
+#2 cách 2
+# from turtle import *
+
+# for edge in range(3, 6):
+#   for i in range (edge):
+#         forward(100)
+#         left(360/edge)
+#         if edge == 3 or edge == 5:
+#             color('blue')
+#         else:
+#             color('red')
+
+# color('red')
+# forward(100)
+
+# for j in range(5):
+#     left(360/6)
+#     forward(100)
+#     color('red')
+
+# left(60)    
+# mainloop()
 
 
 
