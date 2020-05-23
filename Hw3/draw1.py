abc9fdaee6c30e6823953760d4cdeb7b95a4a38d
vinhnq1011 <vinhnq.neu@gmail.com>
2020-05-23 19:07:42 +0700

    innit git

--- Hw3/draw1.py
+++ Hw3/draw1.py
@@ -2,23 +2,14 @@ from turtle import *
 
 
 colors = ['red', 'blue', 'brown', 'yellow', 'grey']
-color(colors[0])
+# color(colors[0])
 mau = 0
 for edge in range(3,8):
+    color(colors[mau])
+    mau = mau + 1
     for i in range (edge):
         forward(100)
         left(360/edge)
-        # print(edge)
-    mau = mau + 1
     print(mau)
-    color(colors[mau])
-    # if edge == 4:
-    #     color(colors[1])
-    # elif edge == 5:
-    #     color(colors[2])
-    # elif edge == 6:
-    #     color(colors[3])
-    # else:
-    #     color(colors[4]) 
-
+    
 mainloop()
