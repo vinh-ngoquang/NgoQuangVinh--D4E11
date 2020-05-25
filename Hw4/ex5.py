0ba38b350285ce0abf6eb88831e212f0ffc77ebe
vinhnq1011 <vinhnq.neu@gmail.com>
2020-05-25 14:48:51 +0700

    init it

---
+++ Hw4/ex5.py
@@ -0,0 +1,7 @@
+congboi = 2
+bar = int(input('How many B barterias are there: '))
+time = int(input('How much time in minutes will we wait: '))
+real_time = time / 2
+
+total = bar * congboi ** (real_time)
+print(int(total))
